e2f96ce744df25e67d47d7ce6772b2df608f0fa6
ducanhnguyen <anhnd.56@gmail.com>
2018-04-24 00:19:51 +0700

    Add jeval

--- src/test/java/testTest.java
+++ src/test/java/testTest.java
@@ -3,6 +3,9 @@ package test.java;
 import org.junit.Assert;
 import org.junit.Test;
 
+import net.sourceforge.jeval.EvaluationException;
+import net.sourceforge.jeval.Evaluator;
+
 public class testTest {
 
 	@Test
@@ -11,7 +14,7 @@ public class testTest {
 	}
 
 	@Test
-	public void testDivide2() {
-		Assert.assertEquals(2, 2);
+	public void testDivide2() throws EvaluationException {
+		Assert.assertEquals("2.0", new Evaluator().evaluate("1+1"));
 	}
 }
